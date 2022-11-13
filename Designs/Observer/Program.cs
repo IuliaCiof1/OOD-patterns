36074722a7b43d7a74766110734cc4e28b780bfc
iuliaCiof <iulia.ciofindac@gmail.com>
2022-11-13 10:24:41 +0200

    added Strategy and Opserver patterns

---
+++ Designs/Observer/Program.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Observer
+{
+    /* Observer pattern - Behavioral pattern
+     * - defines a subscription mechanism  to notify objects about any events that happen to the object they're observing
+     * - allows you to change or take action on a set of objects when and if the state of another object changes
+     * - used when some objects must observe others, but only for a limited time or in specific cases
+     */ 
+
+
+    // **** Subscriber Interface ****
+    //Declares the actual notification interface
+    interface EventListener
+    {
+        void update(Event eventType);
+    }
+
+    // **** Concrete Subscribers ****
+    //They perform some actions in response to notifications issued by the publisher
+
+    //class for customers who chose to be notified via email
+    class EmailMsgListener : EventListener
+    {
+        string email;
+
+        public EmailMsgListener(string email)
+        {
+            this.email = email;
+        }
+
+
+        public void update(Event eventType)
+        {
+            //actually send the email
+            Console.WriteLine($"{email}, you've got a new Email Message for {eventType}");
+        }
+    }
+
+    //class for customers that chose to be notified via push notifications
+    class MobileAppListener:EventListener
+    {
+        string username;
+
+        public MobileAppListener(string username)
+        {
+            this.username = username;
+        }
+
+        public void update(Event eventType)
+        {
+            //actually send the push notification
+            Console.WriteLine($"{username}, you've got a new Notification for {eventType}");
+        }
+    }
+
+    public enum Event { NEW_ITEM, SALE}
+
+    // **** Subject/Publisher ****
+    //When an event happens, the publisher goes over the subscribers and calls the method declared in their interface
+    //Should contain a list of customers and subscribe, unsubscribe and notify methods
+    class NotificationService
+    {
+        //List<EventListener> customers; //- used when there is only one event to subscribe to
+        Dictionary<Event, List<EventListener>> customers;
+
+        public NotificationService()
+        {
+            //customers = new List<EventListener>(); //for only one event
+            customers = new Dictionary<Event, List<EventListener>>();
+
+            foreach(string ev in Enum.GetNames(typeof(Event)))
+            {
+                customers.Add((Event)Enum.Parse(typeof(Event),ev), new List<EventListener>()); //add a new list of EventListener to every item in enum Event
+            }
+        }
+
+        public void subscribe(Event eventType, EventListener listener)
+        {
+            //customers.Add(listener);
+            customers[eventType].Add(listener);
+        }
+
+        public void unsubscribe(Event eventType, EventListener listener)
+        {
+            //customers.Remove(listener);
+            customers[eventType].Remove(listener);
+        }
+
+        public void notify(Event eventType)
+        {
+            //foreach(var listener in customers)
+            //{
+            //    listener.update();
+            //}
+
+            foreach(var listener in customers[eventType])
+            {
+                listener.update(eventType);
+            }
+        }
+
+
+    }
+
+    // **** Client ****
+    //Creates the publisher and the subscriber objects separately and registers the subscribers for publisher updates
+
+    class Store
+    {
+        NotificationService notifyService;
+
+        public Store()
+        {
+            notifyService = new NotificationService();
+        }
+
+        public void newItemPromotion()
+        {
+            notifyService.notify(Event.NEW_ITEM);
+        }
+
+        public void salePromotion()
+        {
+            notifyService.notify(Event.SALE);
+        }
+
+        public NotificationService getNotificationService()
+        {
+            return notifyService;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Store store = new Store();
+
+            var sub = new EmailMsgListener("email1.com");
+            store.getNotificationService().subscribe(Event.NEW_ITEM,sub);
+            store.getNotificationService().subscribe(Event.NEW_ITEM, new MobileAppListener("ana21"));
+            store.getNotificationService().subscribe(Event.SALE, new EmailMsgListener("email2.com"));
+            store.newItemPromotion();
+            store.salePromotion();
+
+            store.getNotificationService().unsubscribe(Event.NEW_ITEM, sub);
+            store.newItemPromotion();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Designs/Strategy/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Strategy
+{
+    /* Strategy pattern - Behavioral pattern
+     * - defines a family of algorithms and puts them in a separate class and makes their objects interchangeable
+     * - used when you want to use different variants of an algorithm wihin an object and be able to switch the algorithm at run time
+     * - used when there are a lot of similar classes that only differ in the way they execute some behavior
+     * - used to isolate bussines logic of a class from the implementation details of algorithms
+     * - useful when your class has a massive conditional statement that switches between variants of the same algorithm
+     * - not useful when you have just a couple of algorithms
+    */
+
+    class CreditCard
+    {
+        public int amount;
+        public int Amount { get { return amount; } set { amount = value; } }
+    }
+
+
+    //**** Strategy Interface ****
+    //Declares operations common to all supported versions of some algorithms
+    interface PaymentStrategy
+    {
+        void pay(int amaount);
+    }
+
+    // **** ConcreteStrategies ****
+    //The interfaces makes them interchangeable in the context
+    class PaymentByCreditCard : PaymentStrategy
+    {
+        CreditCard card;
+
+        public void pay(int amount)
+        {
+            card = new CreditCard();
+            Console.WriteLine($"Paying {amount} with Credit Card");
+            card.Amount = card.Amount - amount;
+        }
+    }
+
+
+    class PaymentByPayPal : PaymentStrategy
+    {
+        string email;
+        string password;
+
+        public void pay(int amount)
+        {
+            email = "email";
+            password = "pass";
+            Console.WriteLine($"Paying {amount} with PayPal");
+        }
+    }
+
+    // **** Context ****
+    //Provides a setter so that the strategy can be swiched at runtime
+    class PaymentService
+    {
+        int cost;
+        PaymentStrategy strategy;
+
+        public void processOrder()
+        {
+            strategy.pay(cost);
+        }
+
+        public void setStrategy(PaymentStrategy strategy)
+        {
+            this.strategy = strategy; 
+        }
+    }
+
+    // **** Client ****
+    //Picks a concrete strategy and passes it to the context
+    class Program 
+    {
+        static void Main(string[] args)
+        {
+            PaymentService service = new PaymentService();
+
+            service.setStrategy(new PaymentByPayPal());
+            service.processOrder();
+            Console.ReadKey();
+        }
+    }
+}
